87c0591a3722f89eb21550d810fd400a8583cc87
RedRyanD <redryand@gmail.com>
2021-07-31 19:56:55 +1000

    Ryan update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+#include <winsock2.h>
 #include "Socket.h"
+#include "Client.h"
+#include "Server.h"
+
+//make a socket, that can connect ot another (client socket)
+//tcp
 
 int main()
 {
+
+
+
 	Net::Socket socket;
 }
