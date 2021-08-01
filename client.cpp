c493345e5d3beb37128ee75f5ebc97123b441525
Your Name <you@example.com>
2021-08-01 13:16:46 +1000

    run format.

--- client.cpp
+++ client.cpp
@@ -6,11 +6,11 @@ using namespace Net;
 
 int main()
 {
-	Socket socket{ IpVersion::Ipv4, Type::TCP };
-	Log(socket.Connect( Ipv4{ 127, 0, 0, 1 }, 8080 ));
+	Socket socket{IpVersion::Ipv4, Type::TCP};
+	Log(socket.Connect(Ipv4{127, 0, 0, 1}, 8080));
 
 	std::string line;
-	while(std::getline(std::cin, line))
+	while (std::getline(std::cin, line))
 	{
 		Log(socket.Send(line.data(), line.size()));
 	}
--- server.cpp
+++ server.cpp
@@ -6,18 +6,18 @@ using namespace Net;
 
 int main()
 {
-	Socket socket{ IpVersion::Ipv4, Type::TCP };
+	Socket socket{IpVersion::Ipv4, Type::TCP};
 
-	Log( socket.Bind(Ipv4{ 127, 0, 0, 1 }, 8080) );
+	Log(socket.Bind(Ipv4{127, 0, 0, 1}, 8080));
 
-	Log( socket.Listen() );
+	Log(socket.Listen());
 
-	auto [accept_status, accepted_socket] = Log( socket.Accept() );
+	auto [accept_status, accepted_socket] = Log(socket.Accept());
 
-	auto [send_status, send_length] = Log( accepted_socket->Send("hello", 5) );
+	auto [send_status, send_length] = Log(accepted_socket->Send("hello", 5));
 
 	char buffer[128];
-	auto [recv_status, recv_length] = Log( accepted_socket->Receive(buffer, 128) );
+	auto [recv_status, recv_length] = Log(accepted_socket->Receive(buffer, 128));
 
 	std::cout << "Received = '";
 	std::cout.write(buffer, recv_length);
